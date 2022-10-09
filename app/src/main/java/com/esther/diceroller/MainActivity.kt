9d1b00995dcd4443072ed1df2fe0a3ce0ca0a09c
jounob <obeiwiljephnie@gmail.com>
2022-10-09 03:12:07 -0300

    Added two dice y reset button

--- app/src/main/java/com/esther/diceroller/MainActivity.kt
+++ app/src/main/java/com/esther/diceroller/MainActivity.kt
@@ -5,39 +5,50 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var ivDice: ImageView
+    private lateinit var ivDice2: ImageView
+    private lateinit var rollButton: Button
+    private lateinit var resetButton: Button
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton: Button = findViewById(R.id.roll_button)
-        val countButton: Button = findViewById(R.id.btnCount)
-        val resetButton: Button = findViewById(R.id.btnReset)
+        rollButton = findViewById(R.id.roll_button)
+        ivDice = findViewById(R.id.ivDice)
+        ivDice2 = findViewById(R.id.ivDice2)
+        //val countButton: Button = findViewById(R.id.prbtnCount)
+        resetButton = findViewById(R.id.btnReset)
 
-        rollButton.setOnClickListener { rollDice() }
+        rollButton.setOnClickListener {
+            rollDice1()
+        }
         //countButton.setOnClickListener { count() }
-         //resetButton.setOnClickListener { reset() }
+        resetButton.setOnClickListener { reset() }
     }
 
-    private fun rollDice(){
-        val randomInt = (1..6).random()
-        val ivDice: ImageView = findViewById(R.id.ivDice)
-       val drawableResource = when(randomInt){
+    private fun rollDice1() {
+        ivDice.setImageResource(getRandomImage())
+        ivDice2.setImageResource(getRandomImage())
+    }
+
+    private fun getRandomImage(): Int {
+        val drawableResource = when ((1..6).random()) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
             4 -> R.drawable.dice_4
             5 -> R.drawable.dice_5
-            else->R.drawable.dice_6
+            else -> R.drawable.dice_6
         }
-        ivDice.setImageResource(drawableResource)
+        return drawableResource
     }
 
-//    private fun count() {
+
+    //    private fun count() {
 //        val nameTV: TextView = findViewById(R.id.tvName)
 //        if(nameTV.text == "Hello World!"){
 //            nameTV.text = "1"
@@ -50,9 +61,9 @@ class MainActivity : AppCompatActivity() {
 //            }
 //        }
 //    }
-//    private fun reset(){
-//        val nameTV: TextView = findViewById(R.id.tvName)
-//        nameTV.text = "0"
-//    }
+    private fun reset() {
+        ivDice.setImageResource(R.drawable.empty_dice)
+        ivDice2.setImageResource(R.drawable.empty_dice)
+    }
 }
 
